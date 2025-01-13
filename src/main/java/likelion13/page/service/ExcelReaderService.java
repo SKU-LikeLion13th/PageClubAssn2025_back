d5715b4925e9a39c7fdd23578b5df2ca2c4c98d2
aitchhan <aitchhan@gmail.com>
2025-01-14 02:00:13 +0900

    [fix] readExcel 1.555.../3

--- src/main/java/likelion13/page/service/ExcelReaderService.java
+++ src/main/java/likelion13/page/service/ExcelReaderService.java
@@ -31,12 +31,12 @@ public class ExcelReaderService {
 
                     // 빈 행 처리 안 함
                     Row row = sheet.getRow(rowIndex);
-                    if (row == null) continue; // 빈 행 처리 안 함
+                    if (row == null) continue; // 전체가 비어있는 행 처리 안 함
 
                     String studentId = getCellValue(row.getCell(columnGroup)); // 학번 추출 (D, G, J열)
                     String studentName = getCellValue(row.getCell(columnGroup + 1)); // 이름 추출 (E, H, K열)
 
-                    if (!studentId.isEmpty() && !studentName.isEmpty()) { // 학번과 이름이 비어 있지 않은 경우만 처리
+                    if (!studentId.isEmpty() && !studentName.isEmpty()) { // 학번과 이름이 비어 있지 않은 경우만 처리 (행 객체는 존재하지만 셀 값이 비어있는 데이터 필터링)
                         Map<String, Object> rowData = new HashMap<>();
                         rowData.put("clubName", clubName);
                         rowData.put("studentId", studentId);
@@ -53,9 +53,10 @@ public class ExcelReaderService {
 
     // 병합된 셀 값 가져오기
     private static String getMergedCellValue(Sheet sheet, int row, int column) {
-        for (int i = 0; i < sheet.getNumMergedRegions(); i++) { // 병합된 영역 탐색
-            CellRangeAddress region = sheet.getMergedRegion(i);
+        for (int i = 0; i < sheet.getNumMergedRegions(); i++) { // 병합된 영역 순회 // sheet.getNumMergedRegions() -> 병합된 영역의 개수 반환
+            CellRangeAddress region = sheet.getMergedRegion(i); // 병합된 셀 영역 중 i번째 영역에 대한 정보
             if (region.isInRange(row, column)) { // 특정 셀이 병합된 영역 안에 있는지 확인
+                // 병합된 영역의 첫 번째 행, 첫 번째 열에서 데이터 읽기 // 병합된 영역에서는 데이터가 항상 첫 번째 셀에 저장되므로...
                 Row mergedRow = sheet.getRow(region.getFirstRow());
                 Cell mergedCell = mergedRow.getCell(region.getFirstColumn());
                 return getCellValue(mergedCell);
