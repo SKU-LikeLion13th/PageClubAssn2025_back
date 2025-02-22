f144593041019ee1d8959c600b77091d6e952325
aitchhan <aitchhan@gmail.com>
2025-02-23 00:00:44 +0900

    [fix] SecurityConfig permitAll path

--- src/main/java/likelion13/page/security/SecurityConfig.java
+++ src/main/java/likelion13/page/security/SecurityConfig.java
@@ -22,10 +22,9 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())       // CORS 설정
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger UI와 API 문서화 경로에 대한 접근을 모든 사용자에게 허용
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/login", "/agree", "/club-scores/**").permitAll() // Swagger UI와 API 문서화 경로에 대한 접근을 모든 사용자에게 허용
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/item-rent/**").hasRole("MEMBER")
-                        .requestMatchers("/login", "/agree", "/club-scores/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 // 간단한 테스트를 위해 csrf토큰 비활성화
